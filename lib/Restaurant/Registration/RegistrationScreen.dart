b1723ce1751f93c9825425fdb462fd6bf1b0a00a
Fazilios <ptfazil02@gmail.com>
2025-08-20 23:32:34 +0530

    fazil

--- lib/Restaurant/Registration/RegistrationScreen.dart
+++ lib/Restaurant/Registration/RegistrationScreen.dart
@@ -11,6 +11,7 @@ class RegistrationScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(14.0),
